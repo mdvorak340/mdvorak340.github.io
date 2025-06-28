7cccdad92e035bc72d4efc77cbec63d1600f1e9c
Mozzie <mdvorak.personal@gmail.com>
2025-06-28 02:44:04 -0500

    added home page

---
+++ home/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, './'),
+  },
+  mode: 'production',
+};
